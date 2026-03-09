0a88d85fb6a3bbb6c3236b80cee791954f8afcfb
joshernz <Jogaa022@osloskolen.no>
2026-03-09 15:06:39 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,66 @@
-console.log("Hello World!")
 
-let a = 5 
-let b = 4
-let c = a + b
+const products = [
+  
+  { kategori: "Drikker", navn: "Melk", pris: 15 },
+  { kategori: "Drikker", navn: "Vann uten kullsyre", pris: 10 },
+  { kategori: "Drikker", navn: "Vann med kullsyre", pris: 12 },
 
-if(c > 10){
-    console.log("C is bigger than 10")
-}  else {
-        console.log("C is smaller than 10")
+  
+  { kategori: "Smoothies", navn: "Mango", pris: 25 },
+  { kategori: "Smoothies", navn: "Appelsin", pris: 25 },
+  { kategori: "Smoothies", navn: "Banan", pris: 25 },
+  { kategori: "Smoothies", navn: "Jordbær", pris: 25 },
+
+  
+  { kategori: "Knekkebrød", navn: "Brunost", pris: 20 },
+  { kategori: "Knekkebrød", navn: "Cheese and Chives", pris: 20 },
+  { kategori: "Knekkebrød", navn: "Sour cream and Onion", pris: 20 },
+
+  
+  { kategori: "Yoghurt", navn: "Vanilje yoghurt liten", pris: 15 },
+  { kategori: "Yoghurt", navn: "Mango yoghurt", pris: 15 },
+  { kategori: "Yoghurt", navn: "Banan yoghurt", pris: 15 }
+]; 
+
+let orders = [];
+
+function addOrder(user, role, productName, antall) {
+ 
+  let product = products.find(p => p.navn === productName);
+  if (!product) {
+    console.log("Produktet finnes ikke!");
+    return;
+  }
+
+  
+  let price = product.pris;
+  if (role === "ansatt") price = price * 0.8; 
+  
+  const order = {
+    user: user,
+    role: role,
+    product: productName,
+    antall: antall,
+    total: price * antall
+  };
+
+  orders.push(order);
+
+  showOrders();
 }
 
+function showOrders() {
+  console.log(" Karoline ser bestillingene ");
+  
+  orders.forEach(order => {
+    console.log(
+      `${order.user} (${order.role}) bestilte ${order.antall} x ${order.product} - Total: ${order.total} kr`
+    );
+  });
 
-
-for(let i = 0; i < 11; i++){
-    console.log(i)
 }
 
-
+addOrder("Josh", "elev", "Melk", 1);
+addOrder("Karwaan", "ansatt", "Banan", 2);
+addOrder("Lala", "elev", "Vanilje yoghurt liten", 1);
+addOrder("Wael", "elev", "Vann med kullsyre", 2);
